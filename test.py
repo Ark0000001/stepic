bd791272bda718d2aa3e1f9f8de28e04e07157c8
ark <yotarona@gmail.com>
2023-11-23 13:17:00 +0500

    1

--- test.py
+++ test.py
@@ -4057,16 +4057,16 @@ from string import ascii_uppercase as ss
 #
 # print(" ".join(sorted_list))
 
-def map(function, items):
-    result = []
-    for item in items:
-        result.append(function(item))
-    return result
-def f(x):
-    return round(x, 2)
-
-numbers = [3.56773, 5.57668, 4.00914, 56.24241, 9.01344, 32.12013, 23.22222, 90.09873, 45.45, 314.1528, 2.71828, 1.41546]
-n=map(f,numbers)
-
-print(*n,sep='\n')
+# def map(function, items):
+#     result = []
+#     for item in items:
+#         result.append(function(item))
+#     return result
+# def f(x):
+#     return round(x, 2)
+#
+# numbers = [3.56773, 5.57668, 4.00914, 56.24241, 9.01344, 32.12013, 23.22222, 90.09873, 45.45, 314.1528, 2.71828, 1.41546]
+# n=map(f,numbers)
+#
+# print(*n,sep='\n')
 
