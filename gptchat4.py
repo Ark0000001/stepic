b613a731f8942d2ee505ee7b9fc46ad6c7dd8420
Admin <rona1078@mail.ru>
2023-11-27 19:13:54 +0500

    обновление

--- gptchat4.py
+++ gptchat4.py
@@ -1,6 +1,71 @@
-from openai import OpenAI
+import openai
 
-client = OpenAI(
-  organization='YOUR_ORG_ID',
+openai.api_key = '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'
+# organization='org-PKAZMf23yXZvOloR9Mepr2wt',
+client = openai.OpenAI(
+  api_key=openai.api_key,
 )
-print(client.models.list())
+
+response = client.images.generate(
+  model="dall-e-3",
+  prompt="нарисуй как в описании: изображен человек за рулем небольшого, необычного, винтажного "
+         "микроавтобуса с характерным красным кузовом и прозрачным куполом. Транспортное средство "
+         "представляет собой трехколесный автомобиль с одним колесом, видимым спереди, "
+         "и обтекаемой, округлой формой кузова. Регистрационный знак и конкретные детали модели "
+         "не относятся к описанию. Водитель одет в очки и что-то похожее на клетчатую рубашку и "
+         "активно управляет автомобилем. Ощущение движения передается через задний план и дорогу, "
+         "которые размыты из-за технологии панорамирования камеры, в то время как автомобиль и "
+         "водитель остаются в фокусе. Обстановка выглядит как солнечный день с ясным небом, "
+         "который подходит для приятной поездки в таком открытом и просторном автомобиле. На "
+         "фотографии запечатлен момент досуга и удовольствия от вождения классического, "
+         "самобытного экземпляра автомобильной истории.",
+  size="1024x1024",
+  quality="standard",
+  n=1,
+)
+
+image_url = response.data[0].url
+print(image_url)
+
+# completion = client.chat.completions.create(
+#   model="gpt-4-1106-preview",
+#   messages=[
+#     {"role": "system", "content": "Вы философ-шутник. Ваш ответ должен быть в формате JSON."},
+#     {"role": "user", "content": "Что будем делать вечером?"}
+#   ],
+#   response_format={"type": "json_object"}
+# )
+#
+# print(completion.choices[0].message.content)
+# system_prompt = "You are an expert analyzing images."
+# response = client.chat.completions.create(
+#     model="gpt-4-vision-preview",
+#     messages=[
+#         {
+#             "role": "system",
+#             "content": [
+#                 {"type": "text", "text": system_prompt},
+#             ],
+#         },
+#         {
+#             "role": "user",
+#             "content": [
+#                 {
+#                     "type": "image_url",
+#                     "image_url": {
+#                         "url":"https://thecode.media/wp-content/uploads/2023/06/1-4.jpg",
+#                     }
+#                 },
+#             ],
+#         },
+#         {
+#             "role": "user",
+#             "content": [
+#                 {"type": "text", "text": "Что изображено на изображении?"},
+#             ],
+#         }
+#     ],
+#     max_tokens=1000,
+# )
+#
+# print(response.choices[0].message.content)
--- test.py
+++ test.py
@@ -5,7 +5,7 @@
 #     print("YES")
 # else:
 #     print("NO")
-
+import random
 # x1 = int(input())
 # y1 = int(input())
 # x2 = int(input())
@@ -4069,4 +4069,448 @@ from string import ascii_uppercase as ss
 # n=map(f,numbers)
 #
 # print(*n,sep='\n')
+
+# def map(function, items):
+#     result = []
+#     for item in items:
+#         result.append(function(item))
+#     return result
+
+# def filter(function, items):
+#     result = []
+#     for item in items:
+#         if function(item):
+#             result.append(item)
+#     return result
+#
+# numbers = [1014, 1321, 675, 1215, 56, 1386, 1385, 431, 1058, 486, 1434, 696, 1016, 1084, 424, 1189, 475, 95, 1434, 1462, 815, 776, 657, 1225, 912, 537, 1478, 1176, 544, 488, 668, 944, 207, 266, 1309, 1027, 257, 1374, 1289, 1155, 230, 866, 708, 144, 1434, 1163, 345, 394, 560, 338, 232, 182, 1438, 1127, 928, 1309, 98, 530, 1013, 898, 669, 105, 130, 1363, 947, 72, 1278, 166, 904, 349, 831, 1207, 1496, 370, 725, 926, 175, 959, 1282, 336, 1268, 351, 1439, 186, 273, 1008, 231, 138, 142, 433, 456, 1268, 1018, 1274, 387, 120, 340, 963, 832, 1127]
+#
+# def f(n):
+#     return (100 <= n <= 999) and n%5==2
+# def f1(n):
+#     return n**3
+# c1=filter(f,numbers)
+# c=map(f1,c1)
+#
+# print(*c,sep='\n')
+
+# def filter_and_map(numbers):
+#     filtered_numbers = filter(lambda x: x % 5 == 2 and 99 < x < 1000, numbers)
+#     cubes = map(lambda x: x ** 3, filtered_numbers)
+#     for cube in cubes:
+#         print(cube)
+#
+# numbers = [1014, 1321, 675, 1215, 56, 1386, 1385, 431, 1058, 486, 1434, 696, 1016, 1084, 424, 1189, 475, 95, 1434, 1462, 815, 776, 657, 1225, 912, 537, 1478, 1176, 544, 488, 668, 944, 207, 266, 1309, 1027, 257, 1374, 1289, 1155, 230, 866, 708, 144, 1434, 1163, 345, 394, 560, 338, 232, 182, 1438, 1127, 928, 1309, 98, 530, 1013, 898, 669, 105, 130, 1363, 947, 72, 1278, 166, 904, 349, 831, 1207, 1496, 370, 725, 926, 175, 959, 1282, 336, 1268, 351, 1439, 186, 273, 1008, 231, 138, 142, 433, 456, 1268, 1018, 1274, 387, 120, 340, 963, 832, 1127]
+# filter_and_map(numbers)
+
+#
+# def reduce(operation, items, initial_value):
+#     acc = initial_value
+#     for item in items:
+#         acc = operation(acc, item)
+#     return acc
+#
+# numbers = [97, 42, 9, 32, 3, 45, 31, 77, -1, 11, -2, 75, 5, 51, 34, 28, 46, 1, -8, 84, 16, 51, 90, 56, 65, 90, 23, 35, 11, -10, 70, 90, 90, 12, 96, 58, -8, -4, 91, 76, 94, 60, 72, 43, 4, -6, -5, 51, 58, 60, 30, 38, 67, 62, 36, 72, 34, 82, 62, -1, 60, 82, 87, 81, -7, 57, 26, 36, 17, 43, 80, 40, 75, 94, 91, 64, 38, 72, 29, 84, 38, 35, 7, 54, 31, 95, 78, 27, 82, 1, 64, 94, 31, 29, -8, 98, 24, 61, 7, 73]
+#
+# def op(n, n1):
+#     return n+n1**2
+# print(reduce(op,numbers,0))
+
+
+# def sum_of_squares(acc, item):
+#     return acc + item**2
+#
+# result = reduce(sum_of_squares, numbers, 0)
+# print(result)
+#
+# # С помощью функций map() и sum()
+# squared_numbers = map(lambda x: x**2, numbers)
+# result = sum(squared_numbers)
+# print(result)
+
+# def map(function, items):
+#     result = []
+#     for item in items:
+#         result.append(function(item))
+#     return result
+# def filter(function, items):
+#     result = []
+#     for item in items:
+#         if function(item):
+#             result.append(item)
+#     return result
+# numbers = [77, 293, 28, 242, 213, 285, 71, 286, 144, 276, 61, 298, 280, 214, 156, 227, 228, 51, -4, 202, 58, 99, 270, 219, 94, 253, 53, 235, 9, 158, 49, 183, 166, 205, 183, 266, 180, 6, 279, 200, 208, 231, 178, 201, 260, -35, 152, 115, 79, 284, 181, 92, 286, 98, 271, 259, 258, 196, -8, 43, 2, 128, 143, 43, 297, 229, 60, 254, -9, 5, 187, 220, -8, 111, 285, 5, 263, 187, 192, -9, 268, -9, 23, 71, 135, 7, -161, 65, 135, 29, 148, 242, 33, 35, 211, 5, 161, 46, 159, 23, 169, 23, 172, 184, -7, 228, 129, 274, 73, 197, 272, 54, 278, 26, 280, 13, 171, 2, 79, -2, 183, 10, 236, 276, 4, 29, -10, 41, 269, 94, 279, 129, 39, 92, -63, 263, 219, 57, 18, 236, 291, 234, 10, 250, 0, 64, 172, 216, 30, 15, 229, 205, 123, -105]
+#
+# def is_two_digit(num):
+#     return abs(num) >= 10 and abs(num) <= 99
+#
+# def is_divisible_by_seven(num):
+#     return num % 7 == 0
+#
+# filtered_numbers = filter(lambda x: is_two_digit(x) and is_divisible_by_seven(x), numbers)
+# squared_numbers = map(lambda x: x ** 2, filtered_numbers)
+# sum_of_squares = sum(squared_numbers)
+#
+# print(sum_of_squares)
+
+# def func_apply(func, values):
+#     return [func(x) for x in values]
+#
+# def add3(x):
+#     return x + 3
+#
+# def mul7(x):
+#     return x * 7
+#
+# print(func_apply(mul7, [1, 2, 3, 4, 5, 6]))
+# print(func_apply(add3, [1, 2, 3, 4, 5, 6]))
+# print(func_apply(str, [1, 2, 3, 4, 5, 6]))
+
+# def func(elem1, elem2, elem3):
+#     return elem1 + elem2 + elem3
+#
+#
+# numbers1 = [1, 2, 3, 4, 5]
+# numbers2 = [10, 20, 30, 40, 50]
+# numbers3 = [100, 200, 300, 400, 500]
+#
+# new_numbers = list(map(func, numbers1, numbers2, numbers3))  #  преобразуем итератор в список
+#
+# print(new_numbers)
+
+# from functools import reduce
+# import operator
+#
+# words = ['Testing ', 'shows ', 'the ', 'presence', ', ', 'not ', 'the ', 'absence ', 'of ', 'bugs']
+# numbers = [1, 2, -6, -4, 3, 9, 0, -6, -1]
+#
+# numbers = list(map(operator.neg, numbers))    #  смена знаков элементов списка
+# concat_words = reduce(operator.add, numbers)             #  конкатенация элементов списка
+#
+#
+# print(concat_words)
+
+# from functools import reduce
+#
+# floats = [4.35, 6.09, 3.25, 9.77, 2.16, 8.88, 4.59, 34.23, 12.12, 4.67, 2.45, 9.32]
+# words = ['racecar', 'akinremi', 'deed', 'temidayo', 'omoseun', 'civic', 'TATTARRATTAT', 'malayalam', 'nun']
+# numbers = [4, 6, 9, 23, 5]
+#
+# # Исправьте этот код
+# map_result = list(map(lambda num: round(num**2,1), floats))
+# filter_result = list(filter(lambda name: name==name[::-1] and len(name)>4, words))
+# reduce_result = reduce(lambda num1, num2: num1 * num2, numbers)
+#
+# print(map_result)
+# print(filter_result)
+# print(reduce_result)
+
+
+# from functools import reduce
+#
+# data = [['Tokyo', 35676000, 'primary'],
+#         ['New York', 19354922, 'nan'],
+#         ['Mexico City', 19028000, 'primary'],
+#         ['Mumbai', 18978000, 'admin'],
+#         ['Sao Paulo', 18845000, 'admin'],
+#         ['Delhi', 15926000, 'admin'],
+#         ['Shanghai', 14987000, 'admin'],
+#         ['Kolkata', 14787000, 'admin'],
+#         ['Los Angeles', 12815475, 'nan'],
+#         ['Dhaka', 12797394, 'primary'],
+#         ['Buenos Aires', 12795000, 'primary'],
+#         ['Karachi', 12130000, 'admin'],
+#         ['Cairo', 11893000, 'primary'],
+#         ['Rio de Janeiro', 11748000, 'admin'],
+#         ['Osaka', 11294000, 'admin'],
+#         ['Beijing', 11106000, 'primary'],
+#         ['Manila', 11100000, 'primary'],
+#         ['Moscow', 10452000, 'primary'],
+#         ['Istanbul', 10061000, 'admin'],
+#         ['Paris', 9904000, 'primary']]
+#
+# # Фильтрация городов с населением более 10000000 человек и со значением 'primary'
+# filtered_cities = filter(lambda x: x[1] > 10000000 and x[2] == 'primary', data)
+#
+# # Получение только названий городов
+# city_names = map(lambda x: x[0], filtered_cities)
+#
+# # Сортировка городов в алфавитном порядке
+# sorted_cities = sorted(city_names)
+#
+# # Соединение названий городов через запятую
+# result = reduce(lambda x, y: x + ', ' + y, sorted_cities)
+#
+# print('Cities:', result)
+
+# func = lambda x: 'True' if x%19==0 or x%13==0 else 'False'
+# print(func(19))
+# print(func(13))
+# print(func(20))
+# print(func(15))
+# print(func(247))
+
+# is_non_negative_num = lambda arg: arg.replace('.', '', 1).isdigit() and float(arg) >= 0
+#
+# print(is_non_negative_num('10.34ab'))
+# print(is_non_negative_num('10.45'))
+# print(is_non_negative_num('-18'))
+# print(is_non_negative_num('-34.67'))
+# print(is_non_negative_num('987'))
+# print(is_non_negative_num('abcd'))
+# print(is_non_negative_num('123.122.12'))
+# print(is_non_negative_num('123.122'))
+
+# is_non_negative_num= lambda x: True if x.replace('.','',1).isdigit() and x[0]!='-' and x.count('.') <=1 else False
+#
+# print(is_non_negative_num('10.34ab'))
+# print(is_non_negative_num('10.45'))
+# print(is_non_negative_num('-18'))
+# print(is_non_negative_num('-34.67'))
+# print(is_non_negative_num('987'))
+# print(is_non_negative_num('abcd'))
+# print(is_non_negative_num('123.122.12'))
+# print(is_non_negative_num('123.122'))
+#
+# is_non_negative_num = lambda q: q.replace('.', '', 1).isdigit()
+
+# is_num = lambda q: q.strip('-').replace('.', '', 1).isdigit() and q.count('-') <=1
+# print(is_num('11-'))
+# print(is_num('10.45'))
+# print(is_num('-18'))
+# print(is_num('-34.67'))
+# print(is_num('987'))
+# print(is_num('abcd'))
+# print(is_num('123.122.12'))
+# print(is_num('-123.122'))
+# print(is_num('--13.2'))
+
+# words = ['beverage', 'monday', 'abroad', 'bias', 'abuse', 'abolish', 'abuse', 'abuse', 'bid', 'wednesday', 'able', 'betray', 'accident', 'abduct', 'bigot', 'bet', 'abandon', 'besides', 'access', 'friday', 'bestow', 'abound', 'absent', 'beware', 'abundant', 'abnormal', 'aboard', 'about', 'accelerate', 'abort', 'thursday', 'tuesday', 'sunday', 'berth', 'beyond', 'benevolent', 'abate', 'abide', 'bicycle', 'beside', 'accept', 'berry', 'bewilder', 'abrupt', 'saturday', 'accessory', 'absorb']
+#
+# print(*sorted(filter(lambda x: len(x)==6,words)))
+
+# numbers = [46, 61, 34, 17, 56, 26, 93, 1, 3, 82, 71, 37, 80, 27, 77, 94, 34, 100, 36, 81, 33, 81, 66, 83, 41, 80, 80, 93, 40, 34, 32, 16, 5, 16, 40, 93, 36, 65, 8, 19, 8, 75, 66, 21, 72, 32, 41, 59, 35, 64, 49, 78, 83, 27, 57, 53, 43, 35, 48, 17, 19, 40, 90, 57, 77, 56, 80, 95, 90, 27, 26, 6, 4, 23, 52, 39, 63, 74, 15, 66, 29, 88, 94, 37, 44, 2, 38, 36, 32, 49, 5, 33, 60, 94, 89, 8, 36, 94, 46, 33]
+#
+# filtered_numbers = list(filter(lambda x: not (x % 2 != 0 and x > 47), numbers))
+# mapped_numbers = list(map(lambda x: x // 2 if x % 2 == 0 else x, filtered_numbers))
+#
+# print(*mapped_numbers)
+
+# data = [(19542209, 'New York'), (4887871, 'Alabama'), (1420491, 'Hawaii'), (626299, 'Vermont'), (1805832, 'West Virginia'), (39865590, 'California'), (11799448, 'Ohio'), (10711908, 'Georgia'), (10077331, 'Michigan'), (10439388, 'Virginia'), (7705281, 'Washington'), (7151502, 'Arizona'), (7029917, 'Massachusetts'), (6910840, 'Tennessee')]
+#
+# s=sorted(data,key=lambda x: x[1][-1], reverse=True)
+# for i in s:
+#     print(f'{i[1]}: {i[0]}')
+
+# data = ['год', 'человек', 'время', 'дело', 'жизнь', 'день', 'рука', 'раз', 'работа', 'слово', 'место', 'лицо', 'друг', 'глаз', 'вопрос', 'дом', 'сторона', 'страна', 'мир', 'случай', 'голова', 'ребенок', 'сила', 'конец', 'вид', 'система', 'часть', 'город', 'отношение', 'женщина', 'деньги']
+#
+# print(*sorted(data,key=lambda x: (len(x),x)))
+
+# mixed_list = ['tuesday', 'abroad', 'abuse', 'beside', 'monday', 'abate', 'accessory', 'absorb', 1384878, 'sunday', 'about', 454805, 'saturday', 'abort', 2121919, 2552839, 977970, 1772933, 1564063, 'abduct', 901271, 2680434, 'bicycle', 'accelerate', 1109147, 942908, 'berry', 433507, 'bias', 'bestow', 1875665, 'besides', 'bewilder', 1586517, 375290, 1503450, 2713047, 'abnormal', 2286106, 242192, 701049, 2866491, 'benevolent', 'bigot', 'abuse', 'abrupt', 343772, 'able', 2135748, 690280, 686008, 'beyond', 2415643, 'aboard', 'bet', 859105, 'accident', 2223166, 894187, 146564, 1251748, 2851543, 1619426, 2263113, 1618068, 'berth', 'abolish', 'beware', 2618492, 1555062, 'access', 'absent', 'abundant', 2950603, 'betray', 'beverage', 'abide', 'abandon', 2284251, 'wednesday', 2709698, 'thursday', 810387, 'friday', 2576799, 2213552, 1599022, 'accept', 'abuse', 'abound', 1352953, 'bid', 1805326, 1499197, 2241159, 605320, 2347441]
+#
+# max_value = max(filter(lambda x: isinstance(x, int), mixed_list))
+# print(max_value)
+
+# mixed_list = ['beside', 48, 'accelerate', 28, 'beware', 'absorb', 'besides', 'berry', 15, 65, 'abate', 'thursday', 76, 70, 94, 35, 36, 'berth', 41, 'abnormal', 'bicycle', 'bid', 'sunday', 'saturday', 87, 'bigot', 41, 'abort', 13, 60, 'friday', 26, 13, 'accident', 'access', 40, 26, 20, 75, 13, 40, 67, 12, 'abuse', 78, 10, 80, 'accessory', 20, 'bewilder', 'benevolent', 'bet', 64, 38, 65, 51, 95, 'abduct', 37, 98, 99, 14, 'abandon', 'accept', 46, 'abide', 'beyond', 19, 'about', 76, 26, 'abound', 12, 95, 'wednesday', 'abundant', 'abrupt', 'aboard', 50, 89, 'tuesday', 66, 'bestow', 'absent', 76, 46, 'betray', 47, 'able', 11]
+#
+# # d=sorted(filter(lambda x: isinstance(x, int),mixed_list))
+# # s=list(filter(lambda x: isinstance(x, str),mixed_list))
+# # s1=sorted(s,key=lambda x: (x))
+# # print(*(d+s1))
+#
+# print(*sorted(mixed_list, key=lambda x: isinstance(x, int),reverse=True))
+
+# print(*map(lambda x: str(255 - int(x)),input().split()))
+
+
+# from functools import reduce
+#
+# # def evaluate(coefficients, x):
+# #     n = len(coefficients) - 1
+# #     exponent = map(lambda i: x ** (n - i), range(n+1))
+# #     terms = map(lambda a, b: a * b, coefficients, exponent)
+# #     result = reduce(lambda a, b: a + b, terms)
+# #     return result
+#
+# def evaluate(coefficients, x):
+#     e=list(range(len(coefficients)-1,0,-1))+[0]
+#     m=list(map(lambda a,e,x1: a*(x1**e),coefficients,e,[x]*len(coefficients)))
+#     result=reduce(lambda a,b: a+b, m)
+#     return result
+#
+# # считываем коэффициенты и значение x
+# coefficients = list(map(int, input().split()))
+# x = int(input())
+#
+# # вызываем функцию evaluate и выводим результат
+# print(evaluate(coefficients, x))
+
+
+# countries = ['Russia', 'USA', 'UK', 'Germany', 'France', 'India']
+# capitals = ['Moscow', 'Washington', 'London', 'Berlin', 'Paris', 'Delhi']
+# population = [145_934_462, 331_002_651, 80_345_321, 67_886_011, 65_273_511, 1_380_004_385]
+# for (c,ca,p) in zip(countries,capitals,population):
+#     print(f'{ca} is the capital of {c}, population equal {p} people.')
+
+# abscissas = input().split()
+# ordinates = input().split()
+# applicates = input().split()
+# points = list(zip(abscissas, ordinates, applicates))
+# R = 2
+# result = all(float(x)**2 + float(y)**2 + float(z)**2 <= R**2 for x, y, z in points)
+# print(result)
+
+# ip_address = input().split('.')
+#
+# valid = all(int(octet) >= 0 and int(octet) <= 255 if octet.isdigit() else False for octet in ip_address)
+#
+# print(valid)
+#
+# print(all(map(lambda n: n.isdigit() and 0 <= int(n) <= 255, input().split('.'))))
+
+# a = int(input())
+# b = int(input())
+#
+# result = []
+# for num in range(a, b+1):
+#     if any(digit == '0' for digit in str(num)):
+#         continue
+#     if all(num % int(digit) == 0 for digit in str(num)):
+#         result.append(str(num))
+#
+# print(' '.join(result))
+
+# s = input()
+# if len(s) >= 7 and any(c.isdigit() for c in s) and any(c.isupper() for c in s) and any(c.islower() for c in s):
+#     print("YES")
+# else:
+#     print("NO")
+
+# n = int(input())
+# flag = True
+# for _ in range(n):
+#     k = int(input())
+#     grades = []
+#     for _ in range(k):
+#         surname, grade = input().split()
+#         grades.append(int(grade))
+#     if not any(grade == 5 for grade in grades):
+#         flag = False
+#         break
+# if flag:
+#     print("YES")
+# else:
+#     print("NO")
+
+# def display(**kwargs):
+#     for i in kwargs:
+#         print(i, end=' ')
+#
+# display(emp='Kelly', salary=9000)
+
+# def generate_letter(mail, name, date, time, place, teacher='Тимур Гуев', number='17'):
+#     return f'''To: {mail}
+# Приветствую, {name}!
+# Вам назначен экзамен, который пройдет {date}, в {time}.
+# По адресу: {place}.
+# Экзамен будет проводить {teacher} в кабинете {number}.
+# Желаем удачи на экзамене!'''
+#
+# print(generate_letter('lara@yandex.ru', 'Лариса', '10 декабря', '12:00', 'Часова 23, корпус 2'))
+# print()
+# print(generate_letter('lara@yandex.ru', 'Лариса', '10 декабря', '12:00',
+#                       'Часова 23, корпус 2', 'Василь Ярошевич', 23))
+
+# def pretty_print(data, side='-', delimiter='|'):
+#     row = ''
+#     for item in data:
+#         row += f" {delimiter} {str(item)}"
+#     row = row.strip()
+#
+#     print(f" {side * (len(row))}")
+#     print(f"{row} {delimiter}")
+#     print(f" {side * (len(row))}")
+#
+# pretty_print([1, 2, 10, 23, 123, 3000])
+# pretty_print(['abc', 'def', 'ghi', '12345'])
+# pretty_print(['abc', 'def', 'ghi'], side='*')
+# pretty_print(['abc', 'def', 'ghi'], delimiter='#')
+# pretty_print(['abc', 'def', 'ghi'], side='*', delimiter='#')
+
+# data = [['p', 'y', 't', 'h', 'o', 'n'], ['s', 't', 'e', 'p', 'i', 'k']]
+# result = list(map(lambda x: '.'.join(x), data))
+# print(result[1])
+
+
+# def concat(*args, sep=' '):
+#     return sep.join(str(arg) for arg in args)
+# print(concat('hello', 'python', 'and', 'stepik'))
+# print(concat('hello', 'python', 'and', 'stepik', sep='*'))
+# print(concat('hello', 'python', sep='()()()'))
+# print(concat('hello', sep='()'))
+# print(concat(1, 2, 3, 4, 5, 6, 7, 8, 9, sep='$$'))
+
+
+# from functools import reduce
+# def product_of_odds(data):
+#     return reduce(lambda x, y: x * y, filter(lambda x: x % 2 == 1, data),1)
+
+# words = 'the world is mine take a look what you have started'.split()
+#
+# print(*map(lambda x: f'"{x}"',words))
+
+# numbers = [(10, -2, 3, 4), (-13, 56), (1, 9, 2), (-1, -9, -45, 32), (-1, 5, 1), (17, 0, 1), (0, 1), (3,), (39, 12), (11, -23), (10, -100, 21, 32), (3, -8), (1, 1)]
+#
+# sorted_numbers = sorted(numbers, key=lambda x: sum(x) / len(x), reverse=True)
+#
+# print(sorted_numbers)
+
+# def arithmetic_operation(operation):
+#     if operation == '+':
+#         return lambda x, y: x + y
+#     elif operation == '-':
+#         return lambda x, y: x - y
+#     elif operation == '*':
+#         return lambda x, y: x * y
+#     elif operation == '/':
+#         return lambda x, y: x / y
+#
+# add = arithmetic_operation('+')
+# div = arithmetic_operation('/')
+# print(add(10, 20))
+# print(div(20, 5))
+
+# s = "cate Frog cat FROGs bee CATERS mouse cATwalk dolphin mOus Cats CatAlo"
+# print(' '.join(sorted(s.split(), key=str.lower)))
+
+# n = int(input())
+# words = []
+# for _ in range(n):
+#     word = input()
+#     hematria = sum(ord(letter) - ord('A') for letter in word.upper())
+#     words.append((word, hematria))
+#
+# words.sort(key=lambda x: (x[1],x[0]))
+# for word, _ in words:
+#     print(word)
+
+# n = int(input())
+# ip_addresses = []
+# for _ in range(n):
+#     ip_addresses.append(input())
+#
+# ip_addresses.sort(key=lambda x: sum(int(b) * 256 ** (3 - i) for i, b in enumerate(x.split('.'))))
+# for ip in ip_addresses:
+#     print(ip)
+
+
+prices_file = open('prices.txt', 'r')
+total_cost = 0
+
+for line in prices_file:
+    product, quantity, price = line.strip().split('\t')
+    total_cost += int(quantity) * int(price)
+
+print(total_cost)
+prices_file.close()
 
